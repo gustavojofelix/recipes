8805908201178c5a627bf7adbf1652934967bfe4
Gustavo Felix <gustavojofelix@gmail.com>
2021-01-08 21:55:44 +0200

    Create the basic screens and install navigation

---
+++ screens/MealCategoryScreen.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const MealCategoryScreen = (props) => {
+  <View style={styles.screen}>
+    <Text>The Meal Category Screen Screen</Text>
+  </View>;
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default MealCategoryScreen;
